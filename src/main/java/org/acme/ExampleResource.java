fc9d84a16e0777b39bc064a483952bf1ae0223f2
Rishabh Singh <rishabhsvats@gmail.com>
2025-06-16 13:02:02 +0530

    Update ExampleResource.java

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -30,7 +30,7 @@ public class ExampleResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return "Hello RESTEasy!!";
     }
 
     @GET
